1de7be9dbf5d82ecb70d69e5524c1c5ba17195d1
Chris DiMaio <chrisdimaio@gmail.com>
2020-09-03 02:15:05 -0400

    Added unicode character

--- src/main/java/io/chrisdima/cards/evaluator/Card.java
+++ src/main/java/io/chrisdima/cards/evaluator/Card.java
@@ -8,10 +8,12 @@ public class Card implements Comparable<Card>{
     @Setter
     private int rank;
     private final int suit;
+    private final String unicodeChar;
 
     public Card(int rank, int suit){
         this.rank = rank;
         this.suit = suit;
+        this.unicodeChar = Unicode.map(rank,suit);
     }
 
     @Override
