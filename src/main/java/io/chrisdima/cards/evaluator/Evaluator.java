e23d3be74d5e344ca5eefe3f8ffeca8414ed0cdb
Chris DiMaio <chrisdimaio@gmail.com>
2020-09-03 00:12:50 -0400

    Renamed SET to THREE_OF_KIND

--- src/main/java/io/chrisdima/cards/evaluator/Evaluator.java
+++ src/main/java/io/chrisdima/cards/evaluator/Evaluator.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class Evaluator {
     private static final List<Long> QUADS = Arrays.asList(1L, 4L);
     private static final List<Long> BOAT = Arrays.asList(2L, 3L);
-    private static final List<Long> SET = Arrays.asList(3L, 1L, 1L);
+    private static final List<Long> THREE_OF_KIND = Arrays.asList(3L, 1L, 1L);
     private static final List<Long> TWO_PAIR = Arrays.asList(2L, 2L, 1L);
 
     public static void main( String[] args ){
@@ -56,7 +56,7 @@ public class Evaluator {
                 hand.setBoat(true);
             }
         } else if(hand.getCounts().size() == 3){
-            if(hand.getCounts().equals(new ArrayList<>(SET))){
+            if(hand.getCounts().equals(new ArrayList<>(THREE_OF_KIND))){
                 hand.setThreeOfAKind(true);
             }else if(hand.getCounts().equals(new ArrayList<>(TWO_PAIR))){
                 hand.setTwoPair(true);
