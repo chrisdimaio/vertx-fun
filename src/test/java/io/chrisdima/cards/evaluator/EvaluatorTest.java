e37e89edde93ab3617c96a040a6e4699b227d8e4
Chris DiMaio <chrisdimaio@gmail.com>
2020-09-09 23:55:57 -0400

    Added tests

--- src/test/java/io/chrisdima/cards/evaluator/EvaluatorTest.java
+++ src/test/java/io/chrisdima/cards/evaluator/EvaluatorTest.java
@@ -1,11 +1,11 @@
 package io.chrisdima.cards.evaluator;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class EvaluatorTest {
     @Test
@@ -26,7 +26,40 @@ public class EvaluatorTest {
 
         Hand highHand = Evaluator.createHand(high);
         Hand lowHand = Evaluator.createHand(low);
-//        assertTrue(Evaluator.compareHand(highHand, lowHand));
-//        assertFalse(Evaluator.compareHand(lowHand, highHand));
+
+        ArrayList<Hand> hands = new ArrayList<>();
+        hands.add(highHand);
+        hands.add(lowHand);
+
+        Hand winner = Evaluator.winner(hands);
+
+        assertEquals(highHand, winner);
+        assertNotEquals(lowHand, winner);
+    }
+
+    @Test
+    public void pairVSAceHigh(){
+        ArrayList<Card> pair = new ArrayList<>();
+        pair.add(new Card(Rank.KING, Suit.HEARTS));
+        pair.add(new Card(Rank.QUEEN, Suit.SPADES));
+        pair.add(new Card(Rank.QUEEN, Suit.HEARTS));
+        pair.add(new Card(Rank.SEVEN, Suit.SPADES));
+        pair.add(new Card(Rank.TWO, Suit.SPADES));
+
+        ArrayList<Card> pairAces = new ArrayList<>();
+        pairAces.add(new Card(Rank.ACE, Suit.DIAMONDS));
+        pairAces.add(new Card(Rank.KING, Suit.CLUBS));
+        pairAces.add(new Card(Rank.NINE, Suit.DIAMONDS));
+        pairAces.add(new Card(Rank.THREE, Suit.DIAMONDS));
+        pairAces.add(new Card(Rank.ACE, Suit.HEARTS));
+
+        Hand handPair = Evaluator.createHand(pair);
+        Hand handPairAces = Evaluator.createHand(pairAces);
+
+        ArrayList<Hand> hands = new ArrayList<>();
+        hands.add(handPair);
+        hands.add(handPairAces);
+
+        assertEquals(handPairAces, Evaluator.winner(hands));
     }
 }
