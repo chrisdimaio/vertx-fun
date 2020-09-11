4eb899ad8b4c08d16e8356e7f5a38a4239bfb45a
Chris DiMaio <chrisdimaio@gmail.com>
2020-09-11 00:28:06 -0400

    Removed poker evaluator

--- src/main/java/io/chrisdima/poker/evaluator/Suit.java
+++
@@ -1,8 +0,0 @@
-package io.chrisdima.poker.evaluator;
-
-public class Suit {
-    public static final int CLUBS = 0;
-    public static final int DIAMONDS = 1;
-    public static final int HEARTS = 2;
-    public static final int SPADES = 3;
-}
