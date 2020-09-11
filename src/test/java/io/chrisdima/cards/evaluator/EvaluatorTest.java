5aaa72467dd8e254a793e8f97c8af14affc1e58e
Chris DiMaio <chrisdimaio@gmail.com>
2020-09-10 20:05:20 -0400

    Added card count and straight tests

--- src/test/java/io/chrisdima/cards/evaluator/EvaluatorTest.java
+++ src/test/java/io/chrisdima/cards/evaluator/EvaluatorTest.java
@@ -3,11 +3,30 @@ package io.chrisdima.cards.evaluator;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.*;
 
 public class EvaluatorTest {
+    @Test
+    public void cardCount(){
+        assertThrows(IllegalStateException.class, () -> Evaluator.createHand(new ArrayList<>(List.of(
+                new Card(Rank.TWO, Suit.HEARTS),
+                new Card(Rank.THREE, Suit.HEARTS),
+                new Card(Rank.FOUR, Suit.DIAMONDS),
+                new Card(Rank.FIVE, Suit.CLUBS)
+        ))));
+
+        assertThrows(IllegalStateException.class, () -> Evaluator.createHand(new ArrayList<>(List.of(
+                new Card(Rank.TWO, Suit.HEARTS),
+                new Card(Rank.THREE, Suit.HEARTS),
+                new Card(Rank.FOUR, Suit.DIAMONDS),
+                new Card(Rank.FIVE, Suit.CLUBS),
+                new Card(Rank.SIX, Suit.SPADES),
+                new Card(Rank.EIGHT, Suit.HEARTS)
+        ))));
+    }
+
     @Test
     public void highLowStraight(){
         ArrayList<Card> high = new ArrayList<>();
@@ -61,5 +80,137 @@ public class EvaluatorTest {
         hands.add(handPairAces);
 
         assertEquals(handPairAces, Evaluator.winner(hands));
+    }
+
+    @Test
+    public void StraightAgainstLowStraight(){
+        Hand straight = Evaluator.createHand(new ArrayList<>(List.of(
+                new Card(Rank.TEN, Suit.HEARTS),
+                new Card(Rank.JACK, Suit.HEARTS),
+                new Card(Rank.QUEEN, Suit.DIAMONDS),
+                new Card(Rank.KING, Suit.CLUBS),
+                new Card(Rank.ACE, Suit.SPADES)
+        )));
+
+        Hand lowStraight = Evaluator.createHand(new ArrayList<>(List.of(
+                new Card(Rank.TWO, Suit.HEARTS),
+                new Card(Rank.THREE, Suit.HEARTS),
+                new Card(Rank.FOUR, Suit.DIAMONDS),
+                new Card(Rank.FIVE, Suit.CLUBS),
+                new Card(Rank.SIX, Suit.SPADES)
+        )));
+
+        ArrayList<Hand> againstLowStraight = new ArrayList<>(List.of(straight, lowStraight));
+        assertEquals(straight, Evaluator.winner(againstLowStraight));
+    }
+
+    @Test
+    public void StraightAgainstHighCard(){
+        Hand straight = Evaluator.createHand(new ArrayList<>(List.of(
+                new Card(Rank.TEN, Suit.HEARTS),
+                new Card(Rank.JACK, Suit.HEARTS),
+                new Card(Rank.QUEEN, Suit.DIAMONDS),
+                new Card(Rank.KING, Suit.CLUBS),
+                new Card(Rank.ACE, Suit.SPADES)
+        )));
+
+        Hand highCard = Evaluator.createHand(new ArrayList<>(List.of(
+                new Card(Rank.TWO, Suit.HEARTS),
+                new Card(Rank.FOUR, Suit.HEARTS),
+                new Card(Rank.FIVE, Suit.DIAMONDS),
+                new Card(Rank.SIX, Suit.CLUBS),
+                new Card(Rank.SEVEN, Suit.SPADES)
+        )));
+
+        ArrayList<Hand> againstHighCard = new ArrayList<>(List.of(straight, highCard));
+        assertEquals(straight, Evaluator.winner(againstHighCard));
+    }
+
+    @Test
+    public void StraightAgainstFlush(){
+        Hand straight = Evaluator.createHand(new ArrayList<>(List.of(
+                new Card(Rank.TEN, Suit.HEARTS),
+                new Card(Rank.JACK, Suit.HEARTS),
+                new Card(Rank.QUEEN, Suit.DIAMONDS),
+                new Card(Rank.KING, Suit.CLUBS),
+                new Card(Rank.ACE, Suit.SPADES)
+        )));
+
+        Hand flush = Evaluator.createHand(new ArrayList<>(List.of(
+                new Card(Rank.TWO, Suit.HEARTS),
+                new Card(Rank.FOUR, Suit.HEARTS),
+                new Card(Rank.FIVE, Suit.HEARTS),
+                new Card(Rank.SIX, Suit.HEARTS),
+                new Card(Rank.SEVEN, Suit.HEARTS)
+        )));
+
+        ArrayList<Hand> againstFlush = new ArrayList<>(List.of(straight, flush));
+        assertNotEquals(straight, Evaluator.winner(againstFlush));
+    }
+
+    @Test
+    public void StraightAgainstBoat(){
+        Hand straight = Evaluator.createHand(new ArrayList<>(List.of(
+                new Card(Rank.TEN, Suit.HEARTS),
+                new Card(Rank.JACK, Suit.HEARTS),
+                new Card(Rank.QUEEN, Suit.DIAMONDS),
+                new Card(Rank.KING, Suit.CLUBS),
+                new Card(Rank.ACE, Suit.SPADES)
+        )));
+
+        Hand boat = Evaluator.createHand(new ArrayList<>(List.of(
+                new Card(Rank.TWO, Suit.HEARTS),
+                new Card(Rank.TWO, Suit.SPADES),
+                new Card(Rank.TWO, Suit.DIAMONDS),
+                new Card(Rank.SEVEN, Suit.CLUBS),
+                new Card(Rank.SEVEN, Suit.HEARTS)
+        )));
+
+        ArrayList<Hand> againstBoat = new ArrayList<>(List.of(straight, boat));
+        assertNotEquals(straight, Evaluator.winner(againstBoat));
+    }
+
+    @Test
+    public void StraightAgainstQuads(){
+        Hand straight = Evaluator.createHand(new ArrayList<>(List.of(
+                new Card(Rank.TEN, Suit.HEARTS),
+                new Card(Rank.JACK, Suit.HEARTS),
+                new Card(Rank.QUEEN, Suit.DIAMONDS),
+                new Card(Rank.KING, Suit.CLUBS),
+                new Card(Rank.ACE, Suit.SPADES)
+        )));
+
+        Hand quads = Evaluator.createHand(new ArrayList<>(List.of(
+                new Card(Rank.TWO, Suit.HEARTS),
+                new Card(Rank.TWO, Suit.SPADES),
+                new Card(Rank.TWO, Suit.DIAMONDS),
+                new Card(Rank.TWO, Suit.CLUBS),
+                new Card(Rank.SEVEN, Suit.HEARTS)
+        )));
+
+        ArrayList<Hand> againstQuads = new ArrayList<>(List.of(straight, quads));
+        assertNotEquals(straight, Evaluator.winner(againstQuads));
+    }
+
+    @Test
+    public void StraightAgainstStraightFlush(){
+        Hand straight = Evaluator.createHand(new ArrayList<>(List.of(
+                new Card(Rank.TEN, Suit.HEARTS),
+                new Card(Rank.JACK, Suit.HEARTS),
+                new Card(Rank.QUEEN, Suit.DIAMONDS),
+                new Card(Rank.KING, Suit.CLUBS),
+                new Card(Rank.ACE, Suit.SPADES)
+        )));
+
+        Hand straightFlush = Evaluator.createHand(new ArrayList<>(List.of(
+                new Card(Rank.TWO, Suit.SPADES),
+                new Card(Rank.THREE, Suit.SPADES),
+                new Card(Rank.FOUR, Suit.SPADES),
+                new Card(Rank.FIVE, Suit.SPADES),
+                new Card(Rank.SIX, Suit.SPADES)
+        )));
+
+        ArrayList<Hand> againstStraightFlush = new ArrayList<>(List.of(straight, straightFlush));
+        assertNotEquals(straight, Evaluator.winner(againstStraightFlush));
     }
 }
